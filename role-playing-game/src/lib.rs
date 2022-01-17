75c0f1576b7dc38ceccbccb8a2aae47e57efaeb4
Alair Fonseca <alairfonseca@Alairs-MacBook-Pro.local>
2022-01-17 13:59:08 -0300

    role-playing-game solution

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -1,6 +1,6 @@
 // This stub file contains items which aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
+use std::cmp;
 
 pub struct Player {
     pub health: u32,
@@ -10,10 +10,39 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health > 0 {
+            return None;
+        }
+
+        Some(Player {
+            health: 100,
+            mana: Player::define_mana(self.level),
+            level: self.level,
+        })
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        match self.mana {
+            None => {
+                self.health = self.health - cmp::min(mana_cost, self.health);
+                return 0;
+            },
+            Some(value) => {
+                if value > mana_cost {
+                    self.mana = Some(self.mana.unwrap() - mana_cost);
+                    return mana_cost * 2;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    pub fn define_mana(level: u32) -> Option<u32> {
+        if level >= 10 {
+            return Some(100);
+        }
+
+        None
     }
 }
