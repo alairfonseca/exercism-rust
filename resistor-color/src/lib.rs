179704b5455220cd87b6a119c21be111b0ccd22c
Alair Fonseca <alairfonseca@Alairs-MacBook-Pro.local>
2022-01-17 01:53:17 -0300

    resistor color solution

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,28 +1,53 @@
-#[derive(Debug, PartialEq)]
+use int_enum::IntEnum;
+use enum_iterator::IntoEnumIterator;
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, IntEnum, IntoEnumIterator)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    //         vec![Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White]
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
-pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+impl ResistorColor {
+    fn value(color: ResistorColor) -> usize {
+        match color {
+            ResistorColor::Black => 0,
+            ResistorColor::Brown => 1,
+            ResistorColor::Red => 2,
+            ResistorColor::Orange => 3,
+            ResistorColor::Yellow => 4,
+            ResistorColor::Green => 5,
+            ResistorColor::Blue => 6,
+            ResistorColor::Violet => 7,
+            ResistorColor::Grey => 8,
+            ResistorColor::White => 9,
+        }
+    }
+}
+
+pub fn color_to_value(color: ResistorColor) -> usize {
+    ResistorColor::value(color) 
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::from_int(value as u8) {
+        Ok(r) => format!("{:?}", r),
+        Err(_) => String::from("value out of range"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    let resistor_iter = ResistorColor::into_enum_iter();
+
+    resistor_iter.map(|color| color).collect()
 }
+
